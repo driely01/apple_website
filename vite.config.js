de357d6d59c54de81c004883b90ea67f7395300a
driely01 <elyaagoubi.works@gmail.com>
2024-06-12 15:11:04 +0100

    update: vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-	base: "apple_website/models/",
+	base: "apple_website/models",
   build: {
     sourcemap: true
   }
